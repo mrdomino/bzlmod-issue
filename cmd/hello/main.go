6c3d9c34caeb13cef70ca8e6c00bd7e8a213ecdc
Steven Dee (Jōshin) <mrdomino@stairwell.com>
2024-11-30 15:38:08 -0500

    Add dependency

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/mrdomino/bzlmod-issue/api"
+)
+
 func main() {
+	req := &api.HelloRequest{
+		Name: "World",
+	}
+	fmt.Printf("Hello %s", req.Name)
 }
